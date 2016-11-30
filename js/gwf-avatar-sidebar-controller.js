1260325214cc8ca36e1730fbfbb2be56753170d6
gizmore <gizmore@wechall.net>
2016-11-30 20:42:58 +0100

    cleanup

--- js/gwf-avatar-sidebar-controller.js
+++ js/gwf-avatar-sidebar-controller.js
@@ -6,28 +6,4 @@ controller('AvatarSidebarCtrl', function($scope) {
 		defaultAvatar: '',
 	};
 	
-	$scope.connect = function() {
-		console.log('ConnectCtrl.connect()');
-		WebsocketSrvc.connect(GWF_CONFIG.ws_url);
-	}
-
-	$scope.disconnect = function($event) {
-		console.log('ConnectCtrl.disconnect()', $event);
-		WebsocketSrvc.disconnect(true);
-	};
-	
-	$scope.$on('gws-ws-open', function() {
-		$scope.data.state.bool = true;
-		$scope.data.state.text = 'established';
-		$scope.$apply();
-	});
-
-	$scope.$on('gws-ws-close', function($event) {
-		console.log('ConnectCtrl.$on-gws-ws-close()', $event);
-		$scope.data.state.bool = false;
-		$scope.data.state.text = 'down';
-		WebsocketSrvc.disconnect(false);
-		$scope.$apply();
-	});
-
 });
